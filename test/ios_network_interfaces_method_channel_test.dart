76d8a62a9861d974266484dfc26f0c69c84dd4f0
Grigorii Siabruk <mrjager999@gmail.com>
2024-03-19 15:56:29 +0300

    Add license

--- test/ios_network_interfaces_method_channel_test.dart
+++ test/ios_network_interfaces_method_channel_test.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ios_network_interfaces/ios_network_interfaces_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelIosNetworkInterfaces platform =
-      MethodChannelIosNetworkInterfaces();
   const MethodChannel channel = MethodChannel('ios_network_interfaces');
 
   setUp(() {
